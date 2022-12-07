2ac37b0715f383a47e0b4c7921f797ece4cd93d4
edtasixm06 <ecanet@profeF2G.informatica.escoladeltreball.org>
2022-12-07 10:08:37 +0100

    m06 ssh

--- ssh22:sshfs/startup.sh
+++ ssh22:sshfs/startup.sh
@@ -1,25 +1,25 @@
 #! /bin/bash
 
-useradd -m -s /bin/bash unix01
-useradd -m -s /bin/bash unix02
-useradd -m -s /bin/bash unix03
-echo -e "unix01\nunix01" | passwd unix01
-echo -e "unix02\nunix02" | passwd unix02
-echo -e "unix03\nunix03" | passwd unix03
+for user in unix01 unix02 unix03 unix04 unix05
+do
+  useradd -m -s /bin/bash $user
+  echo -e "$user\n$user" | passwd $user
+done
 
-cp /opt/docker/login.defs /etc/login.defs
 cp /opt/docker/nslcd.conf /etc/nslcd.conf
 cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
 cp /opt/docker/ldap.conf /etc/ldap/ldap.conf
-cp /opt/docker/common-account /etc/pam.d/common-account
-cp /opt/docker/common-auth /etc/pam.d/common-auth
-cp /opt/docker/common-password /etc/pam.d/common-password
+#cp /opt/docker/common-account /etc/pam.d/common-account
+#cp /opt/docker/common-auth /etc/pam.d/common-auth
+#cp /opt/docker/common-password /etc/pam.d/common-password
 cp /opt/docker/common-session /etc/pam.d/common-session
 cp /opt/docker/pam_mount.conf.xml /etc/security/pam_mount.conf.xml
+
 /usr/sbin/nscd
 /usr/sbin/nslcd
 
 mkdir /root/.ssh
+chmod 700 /root/.ssh
 cp /opt/docker/known_hosts /root/.ssh/known_hosts
 
 /bin/bash
